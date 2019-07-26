ef5af8526c7693b880a2d5e030070ce09249b17a
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2019-07-26 21:44:31 +0900

    Revert "addressモデル、テーブル作成"
    This reverts commit 80a05a714e5d3c419d38d566a96d405efb2c199c.

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_120556) do
-
-  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "postal_code", null: false
-    t.integer "prefecture_id", null: false
-    t.string "city", null: false
-    t.string "address_number"
-    t.string "building_name"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_addresses_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2019_07_26_103058) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -45,5 +33,4 @@ ActiveRecord::Schema.define(version: 2019_07_26_120556) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "addresses", "users"
 end
