4a32fb135f42393e8190b57198f1329ae3bf1bbf
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-27 15:59:40 +0900

    statusにデフォルト値を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_27_032436) do
+ActiveRecord::Schema.define(version: 2019_07_27_063242) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
