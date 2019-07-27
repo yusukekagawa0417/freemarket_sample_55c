c0fc736388f34235af7d7bbd13c99b49d45cd1b1
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-27 15:16:28 +0900

    itemとimageにバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,11 @@ class Item < ApplicationRecord
   has_many   :images, dependent: :destroy
 
   accepts_nested_attributes_for :images
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :condition, presence: true
+  validates :shipping_fee, presence: true
+  validates :shipping_date, presence: true
+  validates :price, presence: true
 end
