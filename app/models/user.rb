73b2c2735c3a43c44db46389d90487802dcc3a29
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-27 14:35:26 +0900

    userモデルの全角スペースを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-　has_many :items, dependent: :destroy
+  has_many :items, dependent: :destroy
 end
