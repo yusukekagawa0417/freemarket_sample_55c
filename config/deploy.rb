785329b3d6b9760f3852e54682f773e9b4cac2bc
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-28 12:08:57 +0900

    本番環境のみS3に保存されるよう設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,6 +20,10 @@ set :keep_releases, 5
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
+  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
   BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
   BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
 }
