8594af4479d6847642a42ad36310959bb4408db1
junki-nishio <thun243@yahoo.co.jp>
2019-07-30 18:36:07 +0900

    カテゴリーモデル、テーブル、rake db:seed時に初期設定としてブランド一覧をカラムに追加する処理を作成した。

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Brandテーブルの初期設定
+require "csv"
+brand_array = CSV.read('db/brand.csv')
+registration_array = ["brand"]
+first_flag = true
+
+#brand.csvで取得したデータにはブランドが重複して存在するので別配列に格納し、重複を無くす
+brand_array.each do |brand|
+  registration_array.each_with_index do |registration,i|
+    if first_flag 
+      #初回ループ時のみ、一致確認をせずデータを配列に格納する
+      registration_array[0] = brand[0]
+      first_flag = false
+      break
+    elsif registration.eql?(brand[0])
+      break
+    elsif i == registration_array.size - 1
+      registration_array.push(brand[0])
+    end
+  end
+end
+
+#重複していないブランドデータをBrandテーブルのnameカラムに登録する
+registration_array.each do |registration_name|
+  Brand.create(name: registration_name)
+end
