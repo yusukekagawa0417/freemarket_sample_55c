8bc3f7f16eb8f30ba1a146ed6a27d995c69732f7
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-30 10:57:30 +0900

    ミスの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,11 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @items = Item.where(seller_id: @user.id)
   end
 
   def edit
     @user = User.find(params[:id])
-    @items = Item.where(seller_id: @user.id)
   end
 
   def update
