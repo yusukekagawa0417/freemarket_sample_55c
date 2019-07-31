606ae2ddaf654f5011a14a3e947ca0bec24c7b68
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-31 12:27:48 +0900

    画像複数投稿・プレビュー/削除(js)

--- app/assets/javascripts/sell.js
+++ app/assets/javascripts/sell.js
@@ -1,4 +1,9 @@
 $(document).on('turbolinks:load', function() {
+  var imagelabel1 = $('.o_image-label1')
+  var imagelabel2 = $('.o_image-label2')
+  var imagezone1 = $('.o_image-zone1-parent')
+  var imagezone2 = $('.o_image-zone2-parent')
+
   var images = [];
   var image_files = [];
 
@@ -8,7 +13,7 @@ $(document).on('turbolinks:load', function() {
     var reader = new FileReader();
     
     var img = $('<div class="add_img"><div class="img_area"><img class="image"></div></div>')
-    var btn = $('<div class="btn_wrapper"><a class="btn_edit">編集</a><a class="btn_delete">削除</a></div>')
+    var btn = $('<div class="btn_wrapper"><a class="btn_delete">削除</a></div>')
     img.append(btn);
     
     reader.onload = function(e) {
@@ -18,15 +23,44 @@ $(document).on('turbolinks:load', function() {
     reader.readAsDataURL(file);
     images.push(img);
     
-    $('#preview').text('');
-    $.each(images, function(i, image){
-      image.data('image', i);
-      $('#preview').append(image);
-    })
+    if (images.length <= 4) {
+      $('#preview1').text('');
+      $.each(images, function(i, image){
+        image.data('image', i);
+        $('#preview1').append(image);
+      })
+      imagelabel1.css({
+        width: `calc(100% - (20% * ${images.length})`
+      })
+    } else if (images.length == 5) {
+      $('#preview1').text('');
+      $.each(images, function(i, image){
+        image.data('image', i);
+        $('#preview1').append(image);
+      })
+      imagelabel1.css({
+        display: "none"
+      })
+      imagezone2.css({
+        display: "block"
+      })
+    } else {
+      var secondrow_images = images.slice(5)
+      $.each(secondrow_images, function(i, image){
+        image.data('image', i + 5);
+        $('#preview2').append(image);
+      })
+      imagelabel2.css({
+        width: `calc(100% - (20% * ${images.length - 5})`
+      })
+    }
+    if (images.length == 10) {
+      imagelabel2.css({
+        display: "none"
+      })
+    }
 
-    var new_image = $(
-      `<input multiple="multiple" name="image[images][]" data-image=${images.length} type="file" id="item_images_attributes_0_image">`
-    );
+    var new_image = $(`<input multiple="multiple" name="image[images][]" data-image=${images.length} type="file" id="item_images_attributes_0_image">`);
     $('.preview-btn').append(new_image);
   })
 
@@ -34,7 +68,61 @@ $(document).on('turbolinks:load', function() {
     var target_image = $(this).parent().parent();
     var target_image_num = target_image.data('image');
     target_image.remove();
+    images.splice(target_image_num, 1);
     image_files.splice(target_image_num, 1);
+
+    if (images.length == 0) {
+      $('input[type="file"]').data('image', 0)
+    }
+
+    if (images.length <= 4) {
+      $('#preview1').text('');
+      $.each(images, function(i, image){
+        image.data('image', i);
+        $('#preview1').append(image);
+      })
+      imagelabel1.css({
+        display: 'block',
+        width: `calc(100% - (20% * ${images.length})`
+      })
+      imagezone2.css({
+        display: 'none'
+      })
+    } else if (images.length == 5) {
+      $('#preview1').text('');
+      $.each(images, function(i, image){
+        image.data('image', i);
+        $('#preview1').append(image);
+      })
+      imagezone1.css({
+        display: 'block'
+      })
+      imagelabel2.css({
+        width: '100%'
+      })
+      imagelabel1.css({
+        display: 'none'
+      })
+      $('#preview2').text('')
+    } else {
+      var firstrow_images = images.slice(0, 5)
+      $('#preview1').text('')
+      $.each(firstrow_images, function(i, image) {
+        image.data('image', i)
+        $('#preview1').append(image)
+      })
+
+      var secondrow_images = images.slice(5)
+      $.each(secondrow_images, function(i, image){
+        image.data('image', i + 5)
+        $('#preview2').append(image)
+        imagelabel2.css({
+          display: 'block',
+          width: `calc(100% - (20% * ${images.length - 5})`
+        })
+      })
+    }
+
   })
 
   $('#new_item').on('submit', function(e) {
