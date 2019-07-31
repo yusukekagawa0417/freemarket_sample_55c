c203cc49f41471cfb883c835d41180661935b5b0
fumikaobayashi <fumika.obayashi@gmail.com>
2019-07-31 17:38:09 +0900

    fields_forのオプションを追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
   has_many   :images, dependent: :destroy
 
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true
 
   validates :name, presence: true
   validates :description, presence: true
