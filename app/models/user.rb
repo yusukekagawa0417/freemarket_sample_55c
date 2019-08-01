9fb9c1e75ace1ee83af89e5ce12b3b0088627a02
takanaka-1024 <riyokokanaketa@gmail.com>
2019-08-01 12:04:01 +0900

    validates記述復元

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,15 @@ class User < ApplicationRecord
   # has_many :messages, dependent: :destroy
   # has_many :evaluations, dependent: :destroy
 
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: {in: 6..128}
+  validates :nickname, presence: true, length: {maximum: 20}
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :firstname_kana, presence: true
+  validates :lastname_kana, presence: true
+  validates :birthday, presence: true
+  validates :tel, uniqueness: true 
+  validates :customer, presence: true, uniqueness: true 
+
 end
