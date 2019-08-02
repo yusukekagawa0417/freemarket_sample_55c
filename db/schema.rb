aaa49e59f023030b0a93bd3081f04a7bd23512e4
sakamakiyuma <yuma78733@yahoo.co.jp>
2019-08-02 19:16:53 +0900

    bundle install , rake db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_07_31_054153) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -30,7 +29,7 @@ ActiveRecord::Schema.define(version: 2019_07_31_054153) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-  
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
