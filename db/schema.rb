a230f41bad78c1d9a19a41df73885c6dcf75e22e
yusukekagawa0417 <y_kagawa0417@yahoo.co.jp>
2019-08-04 18:47:31 +0900

    その他

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_08_02_072302) do
-
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
