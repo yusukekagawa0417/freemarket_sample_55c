91edccd0b65f758c45d46fc8f64aadb56b3fda19
fumikaobayashi <fumika.obayashi@gmail.com>
2019-08-05 13:38:00 +0900

    出品と編集のエラーメッセージを表示

--- app/assets/javascripts/item_validation.js
+++ app/assets/javascripts/item_validation.js
@@ -1,45 +1,55 @@
 $(window).on('load', function(){
-  var error_image = $('.error_image')
-  var error_name = $('.error_name')
-  var error_description = $('.error_description')
-  var error_category = $('.error_category')
-  var error_condition = $('.error_condition')
-  var error_shipping_fee = $('.error_shipping_fee')
-  var error_shipping_method = $('.error_shipping_method')
-  var error_prefecture_id = $('.error_prefecture_id')
-  var error_shipping_date = $('.error_shipping_date')
-  var error_price = $('.error_price')
+  var name = $('.k_ir_section__inner--form2-input')
+  var description = $('.k_ir_section__inner--form2-textarea')
+  var parent_category = $('#category_parent')
+  var child_category = $('#category_children')
+  var grandchild_category = $('#category_grandchildren')
+  var condition = $('.condition')
+  var shipping_fee = $('#shipping_fee')
+  var shipping_method = $('#shipping_method')
+  var prefecture_id = $('.prefecture_id')
+  var shipping_date = $('.shipping_date')
+  var price = $('#sell-price')
 
-  $('#new_item').on('submit', function(e) {
-    if (error_image.val() == "") {
-      $(error_image).text('画像がありません')
+  $('#new_item, .edit_item').on('submit', function() {
+    if ($('#preview1').children()[0] == undefined) {
+      $('.error_image').text('画像がありません')
     }
-    if (error_name.val() == "") {
-      $(error_name).text('入力してください')
+    if (name.val() == "") {
+      $('.error_name').text('入力してください')
     }
-    if (error_description.val() == "") {
-      $(error_description).text('入力してください')
+    if (name.val().length > 40) {
+      $('.error_name').text('40文字以内で入力してください')
     }
-    if (error_category.val() == "") {
-      $(error_category).text('入力してください')
+    if (description.val() == "") {
+      $('.error_description').text('入力してください')
     }
-    if (error_condition.val() == "") {
-      $(error_condition).text('入力してください')
+    if (description.val().length > 1000) {
+      $('.error_description').text('1000文字以内で入力してください')
     }
-    if (error_shipping_fee.val() == "") {
-      $(error_shipping_fee).text('入力してください')
+    if (parent_category.val() == "" || child_category.val() == "" || grandchild_category.val() == "" ) {
+      $('.error_category').text('選択してください')
     }
-    if (error_shipping_method.val() == "") {
-      $(error_shipping_method).text('入力してください')
+    if (condition.val() == "") {
+      $('.error_condition').text('選択してください')
     }
-    if (error_prefecture_id.val() == "") {
-      $(error_prefecture_id).text('入力してください')
+    if (shipping_fee.val() == "") {
+      $('.error_shipping_fee').text('選択してください')
     }
-    if (error_shipping_date.val() == "") {
-      $(error_shipping_date).text('入力してください')
+    if (shipping_method.val() == "") {
+      $('.error_shipping_method').text('選択してください')
     }
-    if (error_price.val() == "") {
-      $(error_price).text('入力してください')
+    if (prefecture_id.val() == "") {
+      $('.error_prefecture_id').text('選択してください')
+    }
+    if (shipping_date.val() == "") {
+      $('.error_shipping_date').text('選択してください')
+    }
+    if (price.val() == "") {
+      $('.error_price').text('入力してください')
+    }
+    if (price.val() != "" && (price.val() < 300 || price.val() > 9999999)) {
+      $('.error_price').text('300以上9999999以下で入力してください')
     }
   })
 })
