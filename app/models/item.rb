e87c8d07f981a21897edc63bb27ee813f21730be
fumikaobayashi <fumika.obayashi@gmail.com>
2019-08-06 13:30:17 +0900

    seller_idのバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,6 +18,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true
   validates :shipping_date, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :seller_id, presence: true
   validates :status,  presence: true
 
   enum condition: {
