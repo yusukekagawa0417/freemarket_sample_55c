d406fa8dd31e485d82082532a4d72dbfd8011ff8
fumikaobayashi <fumika.obayashi@gmail.com>
2019-08-07 20:18:55 +0900

    itemモデルのテストを日本語化に対応

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,61 +4,61 @@ describe Item do
     it "nameが空だと登録できない" do
       item = build(:item, name: nil)
       item.valid?
-      expect(item.errors[:name]).to include("can't be blank")
+      expect(item.errors[:name]).to include("を入力してください")
     end
 
     it "descriptionが空だと登録できない" do
       item = build(:item, description: nil)
       item.valid?
-      expect(item.errors[:description]).to include("can't be blank")
+      expect(item.errors[:description]).to include("を入力してください")
     end
 
     it "category_idが空だと登録できない" do
       item = build(:item, category_id: nil)
       item.valid?
-      expect(item.errors[:category_id]).to include("can't be blank")
+      expect(item.errors[:category_id]).to include("を入力してください")
     end
 
     it "conditionが空だと登録できない" do
       item = build(:item, condition: nil)
       item.valid?
-      expect(item.errors[:condition]).to include("can't be blank")
+      expect(item.errors[:condition]).to include("を入力してください")
     end
 
     it "shipping_feeが空だと登録できない" do
       item = build(:item, shipping_fee: nil)
       item.valid?
-      expect(item.errors[:shipping_fee]).to include("can't be blank")
+      expect(item.errors[:shipping_fee]).to include("を入力してください")
     end
 
     it "shipping_methodが空だと登録できない" do
       item = build(:item, shipping_method: nil)
       item.valid?
-      expect(item.errors[:shipping_method]).to include("can't be blank")
+      expect(item.errors[:shipping_method]).to include("を入力してください")
     end
 
     it "prefecture_idが空だと登録できない" do
       item = build(:item, prefecture_id: nil)
       item.valid?
-      expect(item.errors[:prefecture_id]).to include("can't be blank")
+      expect(item.errors[:prefecture_id]).to include("を入力してください")
     end
 
     it "shipping_dateが空だと登録できない" do
       item = build(:item, shipping_date: nil)
       item.valid?
-      expect(item.errors[:shipping_date]).to include("can't be blank")
+      expect(item.errors[:shipping_date]).to include("を入力してください")
     end
 
     it "priceが空だと登録できない" do
       item = build(:item, price: nil)
       item.valid?
-      expect(item.errors[:price]).to include("can't be blank")
+      expect(item.errors[:price]).to include("を入力してください")
     end
 
     it "statusが空だと登録できない" do
       item = build(:item, status: nil)
       item.valid?
-      expect(item.errors[:status]).to include("can't be blank")
+      expect(item.errors[:status]).to include("を入力してください")
     end
 
     it "上記のカラムが全て存在すれば登録できる" do
@@ -74,7 +74,7 @@ describe Item do
     it "nameが41文字なら登録できない" do
       item = build(:item, name: "！あいうえおアイウエオあいうえおアイウエオあいうえおアイウエオあいうえおアイウエオ")
       item.valid?
-      expect(item.errors[:name]).to include("is too long (maximum is 40 characters)")
+      expect(item.errors[:name]).to include("は40文字以内で入力してください")
     end
 
     it "descriptionが1000文字なら登録できる" do
@@ -85,7 +85,13 @@ describe Item do
     it "descriptionが1001文字なら登録できない" do
       item = build(:item, description: "！ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもゃやゅゆょよらりるれろゎわゐゑをんぁあぃい")
       item.valid?
-      expect(item.errors[:description]).to include("is too long (maximum is 1000 characters)")
+      expect(item.errors[:description]).to include("は1000文字以内で入力してください")
+    end
+
+    it "priceが数字でなければ登録できない" do
+      item = build(:item, price: "a")
+      item.valid?
+      expect(item.errors[:price]).to include("は数値で入力してください")
     end
 
     it "priceが300なら登録できる" do
@@ -96,7 +102,7 @@ describe Item do
     it "priceが299なら登録できない" do
       item = build(:item, price: 299)
       item.valid?
-      expect(item.errors[:price]).to include("must be greater than or equal to 300")
+      expect(item.errors[:price]).to include("は300以上の値にしてください")
     end
 
     it "priceが9999999なら登録できる" do
@@ -107,7 +113,7 @@ describe Item do
     it "priceが10000000なら登録できない" do
       item = build(:item, price: 10000000)
       item.valid?
-      expect(item.errors[:price]).to include("must be less than or equal to 9999999")
+      expect(item.errors[:price]).to include("は9999999以下の値にしてください")
     end
     
   end
