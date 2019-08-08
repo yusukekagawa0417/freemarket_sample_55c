e0007bef18e0571d576ae392831fb728b031201a
fumikaobayashi <fumika.obayashi@gmail.com>
2019-08-08 14:02:44 +0900

    コメント追加

--- app/assets/javascripts/edit_form.js
+++ app/assets/javascripts/edit_form.js
@@ -1,5 +1,6 @@
 $(window).on('load', function(){
   if (location.href.match(/\/items\/\d+\/edit/)){
+    // 親カテゴリーを表示(選択されているものにselected)
     $.each(gon.category_parents, function(i, parent){
       if (parent.id == gon.category_parent.id) {
         $('#category_parent').append(`<option value="${parent.id}" name="category" selected>${parent.name}</option>`)
@@ -7,6 +8,7 @@ $(window).on('load', function(){
         $('#category_parent').append(`<option value="${parent.id}" name="category">${parent.name}</option>`)
       }
     })
+    // 子カテゴリーを表示(選択されているものにselected)
     $.each(gon.category_children, function(i, child){
       if (child.id == gon.category_child.id) {
         $('#category_children').append(`<option value="${child.id}" name="category" selected>${child.name}</option>`)
@@ -14,6 +16,7 @@ $(window).on('load', function(){
         $('#category_children').append(`<option value="${child.id}" name="category">${child.name}</option>`)
       }
     })
+    // 孫カテゴリーを表示(選択されているものにselected)
     $.each(gon.category_grandchildren, function(i, grandchild){
       if (grandchild.id == gon.category_grandchild.id) {
         $('#category_grandchildren').append(`<option value="${grandchild.id}" name="category" selected>${grandchild.name}</option>`)
@@ -22,6 +25,7 @@ $(window).on('load', function(){
       }
     })
 
+    // ブランドが存在すればブランドをformに表示
     if (gon.brand) {
       $('#brand').val(gon.brand.name)
     }
