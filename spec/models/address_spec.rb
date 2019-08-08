0c4947318d58080c3468c916bcf7bc87a9358252
junki-nishio <thun243@yahoo.co.jp>
2019-08-08 17:45:12 +0900

    addressモデルの試験のfactory_botの修正、テストコードの修正

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -3,23 +3,32 @@ describe Address do
   describe '#create' do
     it "is invalid without a postal_code" do
       address = build(:address, postal_code: "")
-      expect(address.errors[:postal_code]).to include("郵便番号を入力してください")
+      address.valid?
+      expect(address.errors[:postal_code]).to include("を入力してください")
     end
-    end
+
     it "is invalid without a prefecture_id" do
-      address = build(:address, prefectire_id: nil)
-      expect(address.errors[:postal_code]).to include("")
+      address = build(:address, prefecture_id: nil)
+      address.valid?
+      expect(address.errors[:prefecture_id]).to include("を入力してください")
     end
+
     it "is invalid without a city" do
       address = build(:address, city: "")
-      expect(address.errors[:postal_code]).to include("市区町村を入力してください")
+      address.valid?
+      expect(address.errors[:city]).to include("を入力してください")
     end
+
     it "is invalid without a address_number" do
       address = build(:address, address_number: "")
-    expect(address.errors[:postal_code]).t o include("番地を入力してください")
+      address.valid?
+    expect(address.errors[:address_number]).to include("を入力してください")
     end
+
     it "is valid with  all column" do
+      user = create(:user)
       address = build(:address)
+      address.valid?
       expect(address).to be_valid
     end
   end
