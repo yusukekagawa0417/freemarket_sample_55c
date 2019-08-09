ef3efe1a3437f68e2086803f636f06b70a376ff1
takanaka-1024 <riyokokanaketa@gmail.com>
2019-08-09 17:36:22 +0900

    商品削除 確認ウィンドウ表示

---
+++ app/assets/javascripts/delete.js
@@ -0,0 +1,25 @@
+$(function(){
+  $('.t_delete').on('click', function(){
+    $('.t_overlay, .t_modalWindow').fadeIn();
+    locateCenter();
+    $(window).resize(localCenter);
+  });
+
+  $('.t_modalWindow__buttons__btn--cxl').on('click', function(){
+    $('.t_overlay, .t_modalWindow').fadeOut();
+  });
+
+
+  function locateCenter(){
+    let w = $(window).width();
+    let h = $(window).height();
+
+    let cw = $('.t_modalWindow').outerWidth();
+    let ch = $('.t_modalWindow').outerHeight();
+
+    $('.t_modalWindow').css({
+      'left': ((w - cw) / 2) + 'px',
+      'top': ((h- ch) / 2) + 'px'
+    });
+  }
+});
