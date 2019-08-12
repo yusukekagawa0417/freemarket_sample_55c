8a3ada89af81cdb7bc6f21feaa4f3a0fb79b107e
takanaka-1024 <riyokokanaketa@gmail.com>
2019-08-12 17:53:36 +0900

    商品詳細 画像スライド表示

---
+++ app/assets/javascripts/img_slick.js
@@ -0,0 +1,11 @@
+$(function() {
+  $('img.o_image-sub--one').mouseover(function(){
+  var selectedSrc = $(this).attr('src');
+  $('img.o_image').stop().fadeOut(50,
+  function(){
+  $('img.o_image').attr('src', selectedSrc);
+  $('img.o_image').stop().fadeIn(200);
+  }
+  );
+ });
+});
