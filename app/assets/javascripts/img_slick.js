fb00ca71bb5a51d3d73ef6e9a86ab44950cbe217
takanaka-1024 <riyokokanaketa@gmail.com>
2019-08-12 19:37:07 +0900

    Update img_slick.js

--- app/assets/javascripts/img_slick.js
+++ app/assets/javascripts/img_slick.js
@@ -1,11 +1,9 @@
-$(function() {
+$(function(){
   $('img.o_image-sub--one').mouseover(function(){
-  var selectedSrc = $(this).attr('src');
-  $('img.o_image').stop().fadeOut(50,
-  function(){
-  $('img.o_image').attr('src', selectedSrc);
-  $('img.o_image').stop().fadeIn(200);
-  }
-  );
- });
+    var selectedSrc = $(this).attr('src');
+    $('img.o_image').stop().fadeOut(50,function(){
+      $('img.o_image').attr('src', selectedSrc);
+      $('img.o_image').stop().fadeIn(200);
+    });
+  });
 });
