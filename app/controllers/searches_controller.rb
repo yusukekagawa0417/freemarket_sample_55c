01591d06ff57c5339f6f70fab4aecc59f5643f09
junki-nishio <thun243@yahoo.co.jp>
2019-08-14 01:23:07 +0900

    検索機能（あいまい検索）の実装

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,24 +1,33 @@
 class SearchesController < ApplicationController
-  before_action :prepare_detail_search
+  before_action :search_preparation
 
   def fuzzy_search
+    @keyword = params[:q][:name_cont]
+    tmp = Item.search(fuzzy_search_params)
+    #検索結果を本家同様1ページに132件までしか表示しない
+    @items = tmp.result.limit(132)
   end
 
-  #詳細検索結果表示
+#詳細検索結果表示
   def detail_search
     tmp = Item.search(detail_search_params)
     @items = tmp.result.includes(:category, :brand)
   end
 
   private
-
-  #詳細検索用の入力画面表示のために各パラメーター準備
-  def prepare_detail_search
+  #検索の入力画面表示のために各パラメーター準備
+  def search_preparation
     @q = Item.ransack(params[:q])
     @categories = Category.where(ancestry: nil)
     @brands = Brand.all
   end
 
+  #あいまい検索用
+  def fuzzy_search_params
+  params.require(:q).permit(:name_cont)
+  end
+  
+  #詳細検索用
   def detail_search_params
     params.require(:q)
     .permit(:name_cont, 
