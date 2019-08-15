94eaff7e05f95e3ae12f0fe27a51525a8d7d50b4
junki-nishio <thun243@yahoo.co.jp>
2019-08-15 15:01:26 +0900

    a

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,4 +1,6 @@
 class CreditsController < ApplicationController
+  before_action :search_preparation
+  
   def new
     @user = User.find(current_user.id)
     Payjp.api_key = Rails.application.credentials.payjp_secret_key
@@ -9,4 +11,10 @@ class CreditsController < ApplicationController
     @card_brand = @card_information.brand      
     @card_src = "//www-mercari-jp.akamaized.net/assets/img/card/visa.svg?3236810361"
   end
+  
+  private
+    #あいまい検索用の設定読み込み
+    def search_preparation
+      @q = Item.ransack(params[:q])
+    end
 end
