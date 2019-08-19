a3ae2a403a25e4a1d50721275d2a64f625e3ab1b
takanaka-1024 <riyokokanaketa@gmail.com>
2019-08-19 17:16:32 +0900

    商品詳細 サムネイルhover表示

--- app/assets/javascripts/slide_img.js
+++ app/assets/javascripts/slide_img.js
@@ -1,9 +1,14 @@
 $(function(){
-  $('img.o_image-sub--one').mouseover(function(){
+  $('.o_image-sub--one').mouseover(function(){
     var selectedSrc = $(this).attr('src');
-    $('img.o_image').stop().fadeOut(50,function(){
-      $('img.o_image').attr('src', selectedSrc);
-      $('img.o_image').stop().fadeIn(200);
-    });
+    if (selectedSrc.match(/mp4/) || selectedSrc.match(/mov/) || selectedSrc.match(/webm/)) {
+      $('.image_zone').addClass('display_none')
+      $('.video_zone').removeClass('display_none')
+      $('.video_zone').html(`<video src="${selectedSrc}" controls="controls" autobuffer="true" width="300px" height="300px"></video>`)
+    } else {
+      $('.video_zone').addClass('display_none')
+      $('.image_zone').removeClass('display_none')
+      $('.image_zone').html(`<img src="${selectedSrc}" width="300px" height="300px">`)
+    }
   });
 });
