cc748dc9429be9c935220de988bed3e9b701d7a7
junki-nishio <thun243@yahoo.co.jp>
2019-08-20 22:14:57 +0900

    コードを改善した。

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -1,28 +1,22 @@
 $(function(){
   $('.header-bottom__left__list__category').hover(function() {
-    $(this).addClass('active');
-    var category_obj = $('.active').children('.category_list');
+    var category_obj = $(this).children('.category_list');
     category_obj.show();
   }, function() {
-    $(this).removeClass('active');
     $(this).children('.category_list').hide();
   });
 
   $('.parent_list').hover(function() {
-    $(this).addClass('active');
-    var category_obj = $('.active').children('.child_top');
+    var category_obj = $(this).children('.child_top');
     category_obj.show();
   }, function() {
-    $(this).removeClass('active');
     $(this).children('.child_top').hide();
   });
 
   $('.child_list').hover(function() {
-    $(this).addClass('active');
-    var category_obj = $('.active').children('.gchild_top');
+    var category_obj = $(this).children('.gchild_top');
     category_obj.show();
   }, function() {
-    $(this).removeClass('active');
     $(this).children('.gchild_top').hide();
   });
 });
